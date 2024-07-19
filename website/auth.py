bc35b61bc7f620df167e52e9e0481578afa300e4
reemaanver <108041797+reemaanver@users.noreply.github.com>
2024-07-19 20:50:43 +0530

    login done

--- website/auth.py
+++ website/auth.py
@@ -7,7 +7,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
-  return render_template('login.html', boolean=True)
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user  = User.query.filter_by(email = email).first()
+    if user:
+      if check_password_hash(user.password, password):
+        flash('Logged in succesfully!', category='success')
+      else:
+        flash('Incorrect password', category='error')
+    else:
+      flash('Email does not exist', category='error')
+  return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
@@ -21,7 +33,11 @@ def sign_up():
     password1 = request.form.get('password1')
     password2 = request.form.get('password2')
 
-    if len(email) < 4:
+    user = User.query.filter_by(email = email).first()
+
+    if user:
+      flash('Email already exists', category='error')
+    elif len(email) < 4:
       flash('Email must be greater than 3 characters', category='error')
     elif len(first_name) < 2:
       flash('First name must be greater than 1 character', category='error')

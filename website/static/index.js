eac8b176407e301a06ffe113b498874f84f07225
reemaanver <108041797+reemaanver@users.noreply.github.com>
2024-07-20 01:33:37 +0530

    delete note feature added

--- website/static/index.js
+++ website/static/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Close Flash Mssg
 const closeFlash = document.querySelector(".close-flash");
 const flashMssg = document.querySelector(".flash-mssg");
 
@@ -8,3 +9,21 @@ const closeBtn = function () {
 };
 
 closeFlash, addEventListener("click", closeBtn);
+
+// Delete Note
+
+function dltNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+
+document.querySelectorAll(".dlt-note").forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const noteId = e.target.getAttribute("data-id");
+    dltNote(noteId);
+  });
+});
